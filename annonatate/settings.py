4ce7455d6745c32eed26f7ce235ff2cd8229fcd5
Peter Binkley <peter.binkley@ualberta.ca>
2021-07-16 14:46:15 -0600

    Use environment vars for client_id and client_secret in settings.py

--- annonatate/settings.py
+++ annonatate/settings.py
@@ -1,6 +1,9 @@
+import os
+
 filepath = "_annotations"
-client_id = ""
-client_secret = ""
+client_id = os.environ.get('ANNONATATE_CLIENT_ID')
+client_secret = os.environ.get('ANNONATATE_CLIENT_SECRET')
+print(client_id)
 github_repo = "annonatate"
 github_branch = "main"
 uploadfolder="/tmp"
