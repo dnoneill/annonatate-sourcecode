d57f88bf6dc4daa81e3547d60c212dd66f1ff13b
Peter Binkley <peter.binkley@ualberta.ca>
2021-08-04 13:59:57 -0600

    Clean up indentation and diagnostics

--- annonatate/utils/github.py
+++ annonatate/utils/github.py
@@ -5,49 +5,44 @@ import os, json
 import base64
 from flask_github import GitHub
 
-import pdb
-
 class GitHubAnno(GitHub):
-  def setbork(self):
-    self.bork = 'bork'
+    def get_existing(self, session, full_url):
+        payload = {'ref': session['github_branch']}
+        match = False
+        if '/images/' in full_url:
+            full_url, match = full_url.strip('/').rsplit('/', 1)
+        existing = self.raw_request('get',full_url, params=payload).json()
+        if match and type(existing) == list:
+            matches = list(filter(lambda x: x['name'] == match, existing))
+            existing = matches[0] if len(matches) > 0 else matches
+        if 'sha' in existing:
+            return existing['sha']
+        else:
+            return ''
 
-  def get_existing(self, session, full_url):
-      payload = {'ref': session['github_branch']}
-      match = False
-      if '/images/' in full_url:
-          full_url, match = full_url.strip('/').rsplit('/', 1)
-      existing = self.raw_request('get',full_url, params=payload).json()
-      if match and type(existing) == list:
-          matches = list(filter(lambda x: x['name'] == match, existing))
-          existing = matches[0] if len(matches) > 0 else matches
-      if 'sha' in existing:
-          return existing['sha']
-      else:
-          return ''
+    def sendgithubrequest(self, session, filename, annotation, path, order=''):
+        data = self.createdatadict(session, filename, annotation, path, order)
+        response = self.raw_request('put', data['url'], data=json.dumps(data['data'], indent=4))
+        return response
 
-  def sendgithubrequest(self, session, filename, annotation, path, order=''):
-      data = self.createdatadict(session, filename, annotation, path, order)
-      response = self.raw_request('put', data['url'], data=json.dumps(data['data'], indent=4))
-      return response
+    def createdatadict(self, session, filename, text, path, order=''):
+        full_url = os.path.join(session['github_url'], path, filename)
+        sha = self.get_existing(session, full_url)
+        writeordelete = "write" if text != 'delete' else "delete"
+        message = "{} {}".format(writeordelete, filename)
+        text = '---\ncanvas: "{}"\n{}---\n{}'.format(text['target']['source'],order, json.dumps(text, indent=4)) if type(text) != str and type(text) != bytes else text
+        text = text.encode('utf-8') if type(text) != bytes else text
+        data = {"message":message, "content": base64.b64encode(text).decode('utf-8'), "branch": session['github_branch'] }
+        if sha != '':
+            data['sha'] = sha
+        return {'data':data, 'url':full_url}
 
-  def createdatadict(self, session, filename, text, path, order=''):
-      full_url = os.path.join(session['github_url'], path, filename)
-      sha = self.get_existing(session, full_url)
-      writeordelete = "write" if text != 'delete' else "delete"
-      message = "{} {}".format(writeordelete, filename)
-      text = '---\ncanvas: "{}"\n{}---\n{}'.format(text['target']['source'],order, json.dumps(text, indent=4)) if type(text) != str and type(text) != bytes else text
-      text = text.encode('utf-8') if type(text) != bytes else text
-      data = {"message":message, "content": base64.b64encode(text).decode('utf-8'), "branch": session['github_branch'] }
-      if sha != '':
-          data['sha'] = sha
-      return {'data':data, 'url':full_url}
-
-  def updateAnnos(self, session):
-      annotations = session['annotations']
-      try:
-          githubresponse = self.get(session['currentworkspace']['contents_url'].replace('{+path}', filepath))
-          githubfilenames = list(map(lambda x: x['name'], githubresponse))
-          session['annotations'] = list(filter(lambda x: x['filename'].split('/')[-1] in githubfilenames,annotations))
-          return githubresponse
-      finally:
-          return False
+    def updateAnnos(self, session):
+        annotations = session['annotations']
+        try:
+            githubresponse = self.get(session['currentworkspace']['contents_url'].replace('{+path}', filepath))
+            githubfilenames = list(map(lambda x: x['name'], githubresponse))
+            session['annotations'] = list(filter(lambda x: x['filename'].split('/')[-1] in githubfilenames,annotations))
+            return githubresponse
+        finally:
+            return False
--- test/test_github.py
+++ test/test_github.py
@@ -12,55 +12,54 @@ import ast
 
 class TestGitHub(unittest.TestCase):
 
-  def setUp(self):
-    print("setup")
-    self.github = anno.GitHubAnno(None)
-    self.session = {'github_branch': 'gb', 'github_url': 'https://api.github.com/repos/testuser/annonatate/contents'}
+    def setUp(self):
+        self.github = anno.GitHubAnno(None)
+        self.session = {'github_branch': 'gb', 'github_url': 'https://api.github.com/repos/testuser/annonatate/contents'}
 
-    @self.github.access_token_getter
-    def get_token_getter():
-        return 'asdf'
+        @self.github.access_token_getter
+        def get_token_getter():
+            return 'asdf'
 
-  mock_raw_request = Mock()
-  mock_raw_request.return_value.json.return_value = {'test': 'test'}
-  @patch('annonatate.utils.github.GitHubAnno.raw_request', mock_raw_request)
-  def test_search_query_none(self):
-    mock_session = {
-      'github_url': 'https://api.github.com/repos/testuser/annonatate/contents',
-      'github_branch': 'gb'
-    }
+    mock_raw_request = Mock()
+    mock_raw_request.return_value.json.return_value = {'test': 'test'}
+    @patch('annonatate.utils.github.GitHubAnno.raw_request', mock_raw_request)
+    def test_search_query_none(self):
+        mock_session = {
+            'github_url': 'https://api.github.com/repos/testuser/annonatate/contents',
+            'github_branch': 'gb'
+        }
 
-    datadict = self.github.createdatadict(
-      mock_session,
-      'testimage.jpg',
-      'text of image',
-      'images',
-      ''
-    )
-    self.assertEqual(datadict['data']['message'], 'write testimage.jpg')
-    self.assertEqual(datadict['data']['content'], 'dGV4dCBvZiBpbWFnZQ==')
-    self.assertNotIn('sha', datadict['data'])
-    self.assertEqual(datadict['url'], 'https://api.github.com/repos/testuser/annonatate/contents/images/testimage.jpg')
+        datadict = self.github.createdatadict(
+            mock_session,
+            'testimage.jpg',
+            'text of image',
+            'images',
+            ''
+        )
+        self.assertEqual(datadict['data']['message'], 'write testimage.jpg')
+        self.assertEqual(datadict['data']['content'], 'dGV4dCBvZiBpbWFnZQ==')
+        self.assertNotIn('sha', datadict['data'])
+        self.assertEqual(datadict['url'], 'https://api.github.com/repos/testuser/annonatate/contents/images/testimage.jpg')
 
 
-  # with uploaded file: this will result in raw_request returning a sha value
-  mock_raw_request = Mock()
-  mock_raw_request.return_value.json.return_value = {'sha': 'sha-abcdef'}
-  @patch('annonatate.utils.github.GitHubAnno.raw_request', mock_raw_request)
-  def test_search_query_upload(self):
-    mock_session = {
-      'github_url': 'https://api.github.com/repos/testuser/annonatate/contents',
-      'github_branch': 'gb'
-    }
-    datadict = self.github.createdatadict(
-      mock_session,
-      'testimage.jpg',
-      'text of image',
-      'images',
-      ''
-    )
-    self.assertEqual(datadict['data']['sha'], 'sha-abcdef')
+    # with uploaded file: this will result in raw_request returning a sha value
+    mock_raw_request = Mock()
+    mock_raw_request.return_value.json.return_value = {'sha': 'sha-abcdef'}
+    @patch('annonatate.utils.github.GitHubAnno.raw_request', mock_raw_request)
+    def test_search_query_upload(self):
+        mock_session = {
+            'github_url': 'https://api.github.com/repos/testuser/annonatate/contents',
+            'github_branch': 'gb'
+        }
+        datadict = self.github.createdatadict(
+            mock_session,
+            'testimage.jpg',
+            'text of image',
+            'images',
+            ''
+        )
+        self.assertEqual(datadict['data']['sha'], 'sha-abcdef')
 
 if __name__ == '__main__':
-   # begin the unittest.main()
-   unittest.main()
+    # begin the unittest.main()
+    unittest.main()
