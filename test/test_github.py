8d6a3f02d542cd4e765294d01f6801b2306772d0
Peter Binkley <peter.binkley@ualberta.ca>
2021-08-10 10:35:12 -0600

    Clean up test_github; separate the github and session test annotations

--- test/test_github.py
+++ test/test_github.py
@@ -20,9 +20,9 @@ class TestGitHub(unittest.TestCase):
         def get_token_getter():
             return 'asdf'
 
-    mock_raw_request = Mock()
-    mock_raw_request.return_value.json.return_value = {'test': 'test'}
-    @patch('annonatate.utils.github.GitHubAnno.raw_request', mock_raw_request)
+    mock_github_get = Mock()
+    mock_github_get.return_value.json.return_value = {'test': 'test'}
+    @patch('annonatate.utils.github.GitHubAnno.raw_request', mock_github_get)
     def test_search_query_none(self):
         mock_session = {
             'github_url': 'https://api.github.com/repos/testuser/annonatate/contents',
@@ -43,9 +43,9 @@ class TestGitHub(unittest.TestCase):
 
 
     # with uploaded file: this will result in raw_request returning a sha value
-    mock_raw_request = Mock()
-    mock_raw_request.return_value.json.return_value = {'sha': 'sha-abcdef'}
-    @patch('annonatate.utils.github.GitHubAnno.raw_request', mock_raw_request)
+    mock_github_get = Mock()
+    mock_github_get.return_value.json.return_value = {'sha': 'sha-abcdef'}
+    @patch('annonatate.utils.github.GitHubAnno.raw_request', mock_github_get)
     def test_search_query_upload(self):
         mock_session = {
             'github_url': 'https://api.github.com/repos/testuser/annonatate/contents',
@@ -61,24 +61,24 @@ class TestGitHub(unittest.TestCase):
         self.assertEqual(datadict['data']['sha'], 'sha-abcdef')
 
     # test deletion of session annotations that have been deleted in Github repo (e.g. by another user)
-    keep_annotation = { "name": "annotation_to_keep", "filename": "http://test/annotation_to_keep" }
-    delete_annotation =   { "name": "annotation_to_delete", "filename": "http://test/annotation_to_delete" }
+    keep_annotation_github = { "name": "annotation_to_keep" }
+    keep_annotation_session = { "filename": "http://test/annotation_to_keep" }
+    delete_annotation_session = { "filename": "http://test/annotation_to_delete" }
 
-    mock_raw_request = MagicMock()
-    mock_raw_request.return_value = [keep_annotation]
-    @patch('annonatate.utils.github.GitHubAnno.get', mock_raw_request)
+    mock_github_get = MagicMock()
+    mock_github_get.return_value = [keep_annotation_github]
+    @patch('annonatate.utils.github.GitHubAnno.get', mock_github_get)
     def test_updateAnnos(self):
         test_session = {
             'currentworkspace': {
                 'contents_url': 'https://api.github.com/repos/testuser/annonatate/contents/{+path}'
             },
-            'annotations': [self.keep_annotation, self.delete_annotation]
+            'annotations': [self.keep_annotation_session, self.delete_annotation_session]
         }
-        filepath = '_annotations' # TODO: make sure this is assigned properly
+        filepath = '_annotations'
         self.github.updateAnnos(test_session, filepath)
-        self.assertIn('annotations', test_session)
         self.assertEqual(len(test_session['annotations']), 1) # one has been deleted
-        self.assertEqual(test_session['annotations'][0]['name'], self.keep_annotation['name']) # right one has been kept
+        self.assertEqual(test_session['annotations'][0]['filename'], self.keep_annotation_session['filename']) # right one has been kept
 
 if __name__ == '__main__':
     # begin the unittest.main()
