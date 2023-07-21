e7e19e5763071e49579323ace08730103e562826
doneill <doneill@ncsu.edu>
2023-07-21 12:58:36 -0400

    fix large file

--- annonatate/static/assets/js/global.js
+++ annonatate/static/assets/js/global.js
@@ -28,6 +28,9 @@ function UrlExists(url){
             status = true;
             senddata = data;
             return {'status': status, 'data': senddata};
+        }, error: function(err) { 
+            senddata = err.responseText;
+            return {'status': status, 'data': err.responseText};
         }
       });
       return {'status': status, 'data': senddata};
@@ -69,18 +72,23 @@ window.onclick = function(e) {
         document.getElementById('imagerror').innerHTML = '';
         var preview = document.createElement('img');
         preview.style.height = "150px";
-          var files    = document.querySelector('input[type=file]').files;
+        var files = document.querySelector('input[type=file]').files;
         var imgpreview = document.getElementById('imagepreview');
         imgpreview.innerHTML = ''
         for (file of files) {
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.fileName = file.name;
-            reader.onload = (event) => {
-            const fileName = event.target.fileName;
-            const content = event.currentTarget.result;
-            imgpreview.innerHTML += `<div><img style="height: 150px" src="${content}"/>
-            <figcaption style="text-align:right">${fileName}</figcaption></div>`
+            if (file.size > 30453078){
+                imgpreview.innerHTML += `<div><img src="" alt='${file.name} too large to preview'/>
+                <figcaption style="text-align:right">${file.name}</figcaption></div>`
+            } else {
+                const reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.fileName = file.name;
+                reader.onload = (event) => {
+                const fileName = event.target.fileName;
+                const content = event.currentTarget.result;
+                imgpreview.innerHTML = `<div><img style="height: 150px" src="${content}"/>
+                <figcaption style="text-align:right">${fileName}</figcaption></div>` + imgpreview.innerHTML
+            }
             };
             if (!imgpreview.style.marginBottom){
                 imgpreview.style.marginBottom = '20px';
